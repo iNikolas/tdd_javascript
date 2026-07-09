1404ac787eb3119e97bba5f2d51357327b383525
Mykola Lebid <realkolos1@gmail.com>
2026-07-09 23:24:57 +0300

    feat: global exception handler

--- packages/server/src/all-exceptions.filter.ts
+++ packages/server/src/all-exceptions.filter.ts
@@ -1,16 +1,43 @@
-import { Catch, ArgumentsHost } from '@nestjs/common';
-import { BaseExceptionFilter } from '@nestjs/core';
+import {
+  Catch,
+  ArgumentsHost,
+  BadRequestException,
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { DrizzleQueryError } from 'drizzle-orm';
+import { BaseExceptionFilter } from '@nestjs/core';
 
 @Catch()
 export class AllExceptionsFilter extends BaseExceptionFilter {
+  private readonly constraintErrors: Record<string, Error> = {
+    '23505': new ConflictException(
+      'A unique constraint violation occurred. The provided value already exists in the database.',
+    ),
+    '23503': new BadRequestException(
+      'A foreign key constraint violation occurred. The provided value does not exist in the referenced table.',
+    ),
+  };
+
   catch(exception: unknown, host: ArgumentsHost) {
     if (exception instanceof DrizzleQueryError) {
-      // Handle DrizzleQueryError specifically
-      console.log('DrizzleQueryError: ', exception);
-    } else {
-      // Handle other exceptions
-      console.log('Other Exception: ', exception);
+      const dbErrorCode =
+        exception.cause != null &&
+        'code' in exception.cause &&
+        typeof exception.cause.code === 'string'
+          ? exception.cause.code
+          : null;
+
+      if (dbErrorCode && this.constraintErrors[dbErrorCode]) {
+        return super.catch(this.constraintErrors[dbErrorCode], host);
+      }
+
+      return super.catch(
+        new InternalServerErrorException(
+          `An unexpected database error occurred: ${dbErrorCode}`,
+        ),
+        host,
+      );
     }
 
     super.catch(exception, host);
